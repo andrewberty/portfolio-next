67babd9265e92f7d8cb1415fa119d33ed01c9d8d
Andrew Berty <andrew@wedomore.com>
2024-09-14 20:41:52 +0300

    add blur animation to header paragraph

--- components/common/Paragraph.tsx
+++ components/common/Paragraph.tsx
@@ -23,10 +23,11 @@ export default function Paragraph({ children, className }: { children: string; c
 				{words.map((word, index) => (
 					<motion.span
 						key={index}
-						initial={{ opacity: 0 }}
+						initial={{ opacity: 0, filter: 'blur(5px)' }}
 						animate={{
 							opacity:
 								index + 1 <= wordsIndex && index + 4 >= wordsIndex ? 1 : (scrollYProgress.get() / words.length) * 20,
+							filter: index + 1 <= wordsIndex && index + 4 >= wordsIndex ? 'blur(0px)' : 'blur(5px)',
 						}}
 						className={cn('mr-2 text-orange-400 text-center font-thin', {
 							'font-medium': index + 1 <= wordsIndex && index + 4 >= wordsIndex,
