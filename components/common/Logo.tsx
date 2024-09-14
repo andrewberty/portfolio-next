fa86cae346da1f77f0d81238867c89e2bdbb9e15
Andrew Berty <andrew@wedomore.com>
2024-09-14 07:42:41 +0300

    logo as a link to homepage

--- components/common/Logo.tsx
+++ components/common/Logo.tsx
@@ -1,16 +1,19 @@
 'use client'
 import Image from 'next/image'
+import Link from 'next/link'
 
 function Logo() {
 	return (
-		<Image
-			src='/assets/images/beard_logo.png'
-			alt='beard logo'
-			width={20}
-			height={20}
-			className='invert cursor-pointer'
-			onClick={() => window.scrollTo(0, 0)}
-		/>
+		<Link href='/'>
+			<Image
+				src='/assets/images/beard_logo.png'
+				alt='beard logo'
+				width={20}
+				height={20}
+				className='invert cursor-pointer'
+				onClick={() => window.scrollTo(0, 0)}
+			/>
+		</Link>
 	)
 }
 
