1d48b730a73abd52129d0963cd1f3677022a6658
Andrew Berty <andrewgeorgeberty@gmail.com>
2024-11-17 20:56:14 +0200

    adjust animations in landing section

--- src/components/pages/homepage/Landing.tsx
+++ src/components/pages/homepage/Landing.tsx
@@ -14,31 +14,34 @@ function Landing() {
 		offset: ['end end', 'end start'],
 	})
 
-	const rotate = useTransform(scrollYProgress, [0, 1], [0, -10])
-	const opacity = useTransform(scrollYProgress, [0, 0.5], [1, 0])
+	const imageRotate = useTransform(scrollYProgress, [0, 1], [0, -20])
+	const textRotate = useTransform(scrollYProgress, [0, 1], [0, 20])
+	const width = useTransform(scrollYProgress, [0, 1], ['60vw', '100vw'])
+	const opacity = useTransform(scrollYProgress, [0, 0.3], [1, 0])
 
 	return (
 		<motion.header
 			ref={target}
-			className='h-[300dvh] flex relative mb-20 mix-blend-screen overflow-clip justify-center'
-			style={{ rotate, opacity }}>
-			<Image
-				src={HeaderImage}
-				alt='header image'
-				className='sticky top-0 h-screen object-cover opacity-50 pointer-events-none'
-			/>
+			className='h-[300dvh] flex relative mb-20 mix-blend-screen overflow-clip justify-center'>
+			<motion.div style={{ rotate: imageRotate, opacity }}>
+				<Image
+					src={HeaderImage}
+					alt='header image'
+					className='sticky top-0 h-screen object-cover opacity-50 pointer-events-none'
+				/>
+			</motion.div>
 
-			<div className='absolute left-0 w-[60%] h-full bg-gradient-to-r from-black to-transparent' />
+			<motion.div className='absolute left-0 h-full bg-gradient-to-r from-black to-transparent' style={{ width }} />
 
-			<div className='absolute inset-0'>
+			<motion.div className='absolute inset-0' style={{ rotate: textRotate }}>
 				<Paragraph className='md:w-[60%] w-[90%] mx-auto sticky top-[50%] -translate-y-1/2 text-[2.2vh] md:text-4xl justify-center'>
 					Welcome to my digital canvas. I’m Andrew, a front-end web developer with a passion for visual elegance. I
 					transform ideas into captivating realities. In this journey, you will see how artistry and technology come
 					together.
 				</Paragraph>
-			</div>
+			</motion.div>
 
-			<div className='absolute bottom-0 left-0 right-0 h-[10%] bg-gradient-to-t from-black to-transparent' />
+			<div className='absolute bottom-0 w-full h-[10%] bg-gradient-to-t from-black to-transparent' />
 		</motion.header>
 	)
 }
