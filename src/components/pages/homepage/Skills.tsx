3eabc9eee6d6ce67f8170a3e75ad88043c8bd688
Andrew Berty <andrewgeorgeberty@gmail.com>
2025-01-29 19:51:47 +0200

    comment out skills for now

--- src/components/pages/homepage/Skills.tsx
+++ src/components/pages/homepage/Skills.tsx
@@ -2,7 +2,7 @@
 
 import { motion } from 'motion/react'
 import Image from 'next/image'
-import { skills } from '../../../../data'
+import { skills } from '@/src/data/data'
 import SectionTitle from '../../common/SectionTitle'
 import RevealContainer from '@/src/utils/reveal/RevealContainer'
 import { revealFromBottom } from '@/src/utils/reveal/revealVariations'
@@ -12,16 +12,16 @@ function Skills() {
 		<section id='skills'>
 			<SectionTitle>skills</SectionTitle>
 
-			<RevealContainer className='flex justify-center items-center flex-wrap p-12 mb-40'>
-				{skills.map((skill, index) => (
-					<motion.div
-						variants={revealFromBottom}
-						key={index}
-						className='flex justify-center items-center w-24 h-24 bg-slate-400/30 border-2 border-white/20 relative m-4 rounded-lg'>
-						<Image src={skill} alt='skill' height={50} width={50} className='object-contain' />
-					</motion.div>
-				))}
-			</RevealContainer>
+			{/* <RevealContainer className='flex justify-center items-center flex-wrap p-12 mb-40'> */}
+			{/* {skills.map((skill, index) => ( */}
+			{/* 	<motion.div */}
+			{/* 		variants={revealFromBottom} */}
+			{/* 		key={index} */}
+			{/* 		className='flex justify-center items-center w-24 h-24 bg-slate-400/30 border-2 border-white/20 relative m-4 rounded-lg'> */}
+			{/* 		<Image src={skill} alt='skill' height={50} width={50} className='object-contain' /> */}
+			{/* 	</motion.div> */}
+			{/* ))} */}
+			{/* </RevealContainer> */}
 		</section>
 	)
 }
